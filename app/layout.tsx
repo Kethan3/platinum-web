f6a05390834afaa4d54d0a4193557d2069987300
Kethan3 <kethangowda003@gmail.com>
2025-05-05 11:36:49 +0530

    - Initialized TypeScript configuration with strict settings and module resolution.

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import "./globals.css"
+import { PropsWithChildren } from "react";
+
+
+
+const RootLayout = (props : PropsWithChildren) => {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default RootLayout
